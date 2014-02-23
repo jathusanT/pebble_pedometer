74ec88778c4b631adcbd02413a18e12e17107445
Jathusan Thiruchelvanathan <jathusan.t@gmail.com>
2014-02-22 19:26:58 -0800

    added step goal window

--- src/run.c
+++ src/run.c
@@ -7,44 +7,107 @@
 	
 Window *window;
 Window *menu_window;
+Window *set_stepGoal;
+
+ActionBarLayer *stepGoalSetter;
 
 SimpleMenuLayer *pedometer_settings;
 SimpleMenuItem menu_items[5];
 SimpleMenuSection menu_sections[1];
 char *item_names[5] = {"Start", "Step Goal", "Pedometer Theme", "About", "Developed By"};
-char *item_sub[5] = {"Lets Go!", "Not Set", "Current: ", "v1.0-RELEASE", "Jathusan T."};
+char *item_sub[5] = {"Lets Go!", "Not Set", "Current: Light", "v1.0-RELEASE", "Jathusan T, (c) 2014"};
 
 TextLayer *main_message;
 TextLayer *main_message2;
 TextLayer *sub_message;
 TextLayer *sub_message2;
+TextLayer *stepGoalVisualizer;
+
 
 //used for themes
 bool isDark = false;
 bool stepLimitExists = false;
 char *theme = "Light";
-int stepGoal = 1000;
+int stepGoal = 0;
 int pedometerCount = 0;
+
+
+void start_callback(int index, void *ctx){
+	
+}
+
+void info_callback(int index, void *ctx){
+	
+}
+
+void stepGoal_load(Window *window){
+	stepGoalVisualizer = text_layer_create(GRect(0, 0, 150, 150));
+	stepGoalSetter = action_bar_layer_create();
+	text_layer_set_background_color(stepGoalVisualizer, GColorClear);
+	
+	if (isDark){
+		window_set_background_color(set_stepGoal, GColorBlack);
+		text_layer_set_background_color(stepGoalVisualizer, GColorWhite);
+		action_bar_layer_set_background_color(stepGoalSetter, GColorWhite);
+	} else {
+		window_set_background_color(set_stepGoal, GColorWhite);
+		text_layer_set_background_color(stepGoalVisualizer, GColorBlack);
+		action_bar_layer_set_background_color(stepGoalSetter, GColorBlack);
+	}
+	
+	text_layer_set_font(stepGoalVisualizer, fonts_load_custom_font(resource_get_handle(RESOURCE_ID_ROBOTO_LT_30)));
+    layer_add_child(window_get_root_layer(window), (Layer*) stepGoalVisualizer);
+	
+	static char buf[]="123456";
+	snprintf(buf, sizeof(buf), "%d", stepGoal);
+	
+	text_layer_set_text(stepGoalVisualizer, buf);
+	action_bar_layer_add_to_window(stepGoalSetter, set_stepGoal);
+}
+
+void stepGoal_unload(Window *window){
+	//TODO add this later
+}
+
+void stepGoal_callback(int index, void *ctx){
+	set_stepGoal = window_create();
+	window_set_window_handlers(window, (WindowHandlers) {
+      .load = stepGoal_load,
+      .unload = stepGoal_unload,
+    });
+	window_stack_push(set_stepGoal, true);
+	
+	static char buf[]="123456";
+	snprintf(buf, sizeof(buf), "%d", stepGoal); 
+	
+	//marking the layer as dirty, to acknowledge layer change
+	if (stepGoal !=0){
+		menu_items[1].subtitle = buf;
+		layer_mark_dirty(simple_menu_layer_get_layer(pedometer_settings));
+	}
+}
+
+void theme_callback(int index, void *ctx){
+	isDark = !isDark;
+	if(isDark){
+		theme = "Dark";
+	} else {
+		theme = "Light";
+	}
+	char* new_string;
+	new_string = malloc(strlen(theme)+10);
+	strcpy(new_string, "Current: ");
+	strcat(new_string, theme);
+	menu_items[2].subtitle = new_string;
+	
+	//marking the layer as dirty, to acknowledge layer change
+	layer_mark_dirty(simple_menu_layer_get_layer(pedometer_settings));
+}
 
 
 ////////////////////////////////////////////////////////////////////
 // Settings Menu
 ///////////////////////////////////////////////////////////////////
-
-void m_up_click_handler(ClickRecognizerRef recognizer, void *context){
-}
- 
-void m_down_click_handler(ClickRecognizerRef recognizer, void *context){
-}
- 
-void m_select_click_handler(ClickRecognizerRef recognizer, void *context){
-}
-
-void menu_click_config_provider(void *context){
-    window_single_click_subscribe(BUTTON_ID_UP, m_up_click_handler);
-    window_single_click_subscribe(BUTTON_ID_DOWN, m_down_click_handler);
-    window_single_click_subscribe(BUTTON_ID_SELECT, m_select_click_handler);
-}
 
 void setup_menu_items(){
 	for (int i = 0; i < (int)(sizeof(item_names) / sizeof(item_names[0])); i++){
@@ -52,18 +115,18 @@ void setup_menu_items(){
     		.title = item_names[i],
 			.subtitle = item_sub[i],
 		};
-		if (i==1 && stepGoal>0){
-			static char buf[]="123456";
-			snprintf(buf, sizeof(buf), "%d", stepGoal); 
-			menu_items[i].subtitle = buf;
+		
+		//Setting Callbacks
+		if (i==0){
+			menu_items[i].callback = start_callback;
+		} else if (i==1){
+			menu_items[i].callback = stepGoal_callback;
 		} else if (i==2){
-			char* new_string;
-			new_string = malloc(strlen(item_sub[i])+strlen(theme)+1);
-			strcpy(new_string, item_sub[i]);
-			strcat(new_string, theme);
-			menu_items[i].subtitle = new_string;
+			menu_items[i].callback = theme_callback;
+		} else {
+			menu_items[i].callback = info_callback;
 		}
-    }
+	}
 }
 
 void setup_menu_sections(){
@@ -80,15 +143,9 @@ void setup_menu_window(){
       .load = settings_load,
       .unload = settings_unload,
     });
-	
-	window_set_click_config_provider(menu_window, menu_click_config_provider);
 }
 
 void settings_load(Window *window){	
-	if (isDark){
-    	//window_set_background_color(window, GColorBlack);
-	}
-	
 	Layer *layer = window_get_root_layer(menu_window);
 	
 	pedometer_settings = simple_menu_layer_create(layer_get_bounds(layer), menu_window, menu_sections, 1, NULL);
@@ -96,7 +153,7 @@ void settings_load(Window *window){
 }
 
 void settings_unload(Window *window){
-    //empty for now
+    layer_destroy(window_get_root_layer(menu_window));
 }
 
 ////////////////////////////////////////////////////////////////////
@@ -121,14 +178,6 @@ void handleColorSelect(){
         text_layer_set_text_color(sub_message2, GColorWhite);
     }   
 }
-
-void up_click_handler(ClickRecognizerRef recognizer, void *context){
-
-}
- 
-void down_click_handler(ClickRecognizerRef recognizer, void *context){
-
-}
  
 void select_click_handler(ClickRecognizerRef recognizer, void *context){
 	window_stack_pop(true);
@@ -140,8 +189,6 @@ void select_click_handler(ClickRecognizerRef recognizer, void *context){
 
 void click_config_provider(void *context)
 {
-    window_single_click_subscribe(BUTTON_ID_UP, up_click_handler);
-    window_single_click_subscribe(BUTTON_ID_DOWN, down_click_handler);
     window_single_click_subscribe(BUTTON_ID_SELECT, select_click_handler);
 }
 
