2a1b50fd7fde4d35ca74ab24e20cd1de9fcb774f
Jathusan Thiruchelvanathan <jathusan.t@gmail.com>
2014-02-22 21:17:23 -0800

    added ability to set step goal

--- src/run.c
+++ src/run.c
@@ -15,7 +15,7 @@ SimpleMenuLayer *pedometer_settings;
 SimpleMenuItem menu_items[5];
 SimpleMenuSection menu_sections[1];
 char *item_names[5] = {"Start", "Step Goal", "Pedometer Theme", "About", "Developed By"};
-char *item_sub[5] = {"Lets Go!", "Not Set", "Current: Light", "v1.0-RELEASE", "Jathusan T, (c) 2014"};
+char *item_sub[5] = {"Lets Go Running!", "Not Set", "Current: Dark", "v1.0-RELEASE", "Jathusan T, (c) 2014"};
 
 TextLayer *main_message;
 TextLayer *main_message2;
@@ -23,14 +23,11 @@ TextLayer *sub_message;
 TextLayer *sub_message2;
 TextLayer *stepGoalVisualizer;
 
-
 //used for themes
-bool isDark = false;
-bool stepLimitExists = false;
-char *theme = "Light";
+bool isDark = true;
 int stepGoal = 0;
 int pedometerCount = 0;
-
+const int STEP_INCREMENT = 100;
 
 void start_callback(int index, void *ctx){
 	
@@ -40,29 +37,61 @@ void info_callback(int index, void *ctx){
 	
 }
 
+void inc_click_handler(ClickRecognizerRef recognizer, void *context){
+	stepGoal+=STEP_INCREMENT;
+	static char buf[]="123456";
+	snprintf(buf, sizeof(buf), "%d", stepGoal);
+	text_layer_set_text(stepGoalVisualizer, buf);
+	if (stepGoal !=0){
+		menu_items[1].subtitle = buf;
+	}
+	layer_mark_dirty(simple_menu_layer_get_layer(pedometer_settings));
+}
+
+void dec_click_handler(ClickRecognizerRef recognizer, void *context){
+	if (stepGoal >=STEP_INCREMENT){
+		stepGoal-=STEP_INCREMENT;
+		static char buf[]="123456";
+		snprintf(buf, sizeof(buf), "%d", stepGoal);
+		text_layer_set_text(stepGoalVisualizer, buf);
+		if (stepGoal !=0){
+			menu_items[1].subtitle = buf;
+		}
+		layer_mark_dirty(simple_menu_layer_get_layer(pedometer_settings));
+	}
+}
+
+void goal_set_click_config(void *context){
+  window_single_click_subscribe(BUTTON_ID_DOWN, (ClickHandler) dec_click_handler);
+  window_single_click_subscribe(BUTTON_ID_UP, (ClickHandler) inc_click_handler);
+}
+
 void stepGoal_load(Window *window){
-	stepGoalVisualizer = text_layer_create(GRect(0, 0, 150, 150));
 	stepGoalSetter = action_bar_layer_create();
-	text_layer_set_background_color(stepGoalVisualizer, GColorClear);
 	
-	if (isDark){
+	action_bar_layer_add_to_window(stepGoalSetter, set_stepGoal);
+	action_bar_layer_set_click_config_provider(stepGoalSetter, goal_set_click_config);
+	
+	stepGoalVisualizer = text_layer_create(GRect(0, 0, 150, 150));
+	text_layer_set_background_color(stepGoalVisualizer, GColorClear);	
+	text_layer_set_font(stepGoalVisualizer, fonts_load_custom_font(resource_get_handle(RESOURCE_ID_ROBOTO_LT_30)));
+    layer_add_child(window_get_root_layer(window), (Layer*) stepGoalVisualizer);
+
+	static char buf[]="123456";
+	snprintf(buf, sizeof(buf), "%d", stepGoal);
+	text_layer_set_text(stepGoalVisualizer, buf);
+	
+	if(isDark) {
 		window_set_background_color(set_stepGoal, GColorBlack);
-		text_layer_set_background_color(stepGoalVisualizer, GColorWhite);
+		text_layer_set_background_color(stepGoalVisualizer, GColorClear);
+		text_layer_set_text_color(stepGoalVisualizer, GColorWhite);
 		action_bar_layer_set_background_color(stepGoalSetter, GColorWhite);
 	} else {
 		window_set_background_color(set_stepGoal, GColorWhite);
-		text_layer_set_background_color(stepGoalVisualizer, GColorBlack);
+		text_layer_set_background_color(stepGoalVisualizer, GColorClear);
+		text_layer_set_text_color(stepGoalVisualizer, GColorBlack);
 		action_bar_layer_set_background_color(stepGoalSetter, GColorBlack);
 	}
-	
-	text_layer_set_font(stepGoalVisualizer, fonts_load_custom_font(resource_get_handle(RESOURCE_ID_ROBOTO_LT_30)));
-    layer_add_child(window_get_root_layer(window), (Layer*) stepGoalVisualizer);
-	
-	static char buf[]="123456";
-	snprintf(buf, sizeof(buf), "%d", stepGoal);
-	
-	text_layer_set_text(stepGoalVisualizer, buf);
-	action_bar_layer_add_to_window(stepGoalSetter, set_stepGoal);
 }
 
 void stepGoal_unload(Window *window){
@@ -71,29 +100,34 @@ void stepGoal_unload(Window *window){
 
 void stepGoal_callback(int index, void *ctx){
 	set_stepGoal = window_create();
-	window_set_window_handlers(window, (WindowHandlers) {
+	
+	window_set_window_handlers(set_stepGoal, (WindowHandlers) {
       .load = stepGoal_load,
       .unload = stepGoal_unload,
     });
+		
 	window_stack_push(set_stepGoal, true);
-	
+		
 	static char buf[]="123456";
 	snprintf(buf, sizeof(buf), "%d", stepGoal); 
 	
 	//marking the layer as dirty, to acknowledge layer change
 	if (stepGoal !=0){
 		menu_items[1].subtitle = buf;
-		layer_mark_dirty(simple_menu_layer_get_layer(pedometer_settings));
 	}
+	layer_mark_dirty(simple_menu_layer_get_layer(pedometer_settings));
 }
 
 void theme_callback(int index, void *ctx){
-	isDark = !isDark;
+	char *theme = "";
 	if(isDark){
-		theme = "Dark";
-	} else {
+		isDark = false;
 		theme = "Light";
+	} else {
+		isDark = true;
+		theme = "Dark";
 	}
+	
 	char* new_string;
 	new_string = malloc(strlen(theme)+10);
 	strcpy(new_string, "Current: ");
@@ -149,6 +183,7 @@ void settings_load(Window *window){
 	Layer *layer = window_get_root_layer(menu_window);
 	
 	pedometer_settings = simple_menu_layer_create(layer_get_bounds(layer), menu_window, menu_sections, 1, NULL);
+	simple_menu_layer_set_selected_index(pedometer_settings, 0, true);
 	layer_add_child(layer,simple_menu_layer_get_layer(pedometer_settings));
 }
 
@@ -159,25 +194,6 @@ void settings_unload(Window *window){
 ////////////////////////////////////////////////////////////////////
 // Splash Menu
 ///////////////////////////////////////////////////////////////////
-void handleColorSelect(){
-    if (isDark){
-        isDark = false;
-		theme = "Light";
-        window_set_background_color(window, GColorWhite);
-        text_layer_set_text_color(main_message, GColorBlack);
-        text_layer_set_text_color(main_message2, GColorBlack);
-        text_layer_set_text_color(sub_message, GColorBlack);
-        text_layer_set_text_color(sub_message2, GColorBlack);
-    } else {
-        isDark = true;
-		theme = "Dark";
-        window_set_background_color(window, GColorBlack);
-        text_layer_set_text_color(main_message, GColorWhite);
-        text_layer_set_text_color(main_message2, GColorWhite);
-        text_layer_set_text_color(sub_message, GColorWhite);
-        text_layer_set_text_color(sub_message2, GColorWhite);
-    }   
-}
  
 void select_click_handler(ClickRecognizerRef recognizer, void *context){
 	window_stack_pop(true);
